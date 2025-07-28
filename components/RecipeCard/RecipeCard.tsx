93e193d8b3524aaf08a7dc17354db2329ed61fd0
mauryasoni2201 <maurya@krishaweb.com>
2025-07-28 11:46:54 +0530

    Made header responsive.

--- components/RecipeCard/RecipeCard.tsx
+++ components/RecipeCard/RecipeCard.tsx
@@ -8,13 +8,13 @@ const RecipeCard = ({ recipe }: { recipe: RecipeProps }) => {
       <div className="card-wrapper">
         <div className="card-image-and-content">
           <Image fill src={recipe.image} alt={recipe.name} />
-          <Link className="recipe-link" href={`/recipes/${recipe.id}`}></Link>
+          <Link aria-label={recipe.name} className="recipe-link" href={`/recipes/${recipe.id}`}></Link>
           <div className="card-overlay-content">
             <h3 className="h3 title">{recipe.name}</h3>
             <div className="tags">
               {recipe.tags.map((element: string) => (
                 <div key={element} className="tag-wrapper">
-                  <Link className="tag" href={`/recipes/tag/${element.toLowerCase()}`}>
+                  <Link aria-label={recipe.name} className="tag" href={`/recipes/tag/${element.toLowerCase()}`}>
                     {element}
                   </Link>
                 </div>
