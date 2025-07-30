e0e5ec056a8e86f13204b83e8e09fb7f4249baf0
mauryasoni2201 <maurya@krishaweb.com>
2025-07-30 15:06:59 +0530

    Add remove recipe from favorites functionality.

--- components/RecipeListing/RecipeListing.tsx
+++ components/RecipeListing/RecipeListing.tsx
@@ -1,14 +1,26 @@
 import Recipe from "@/models/RecipeProps";
 import RecipeCard from "../RecipeCard/RecipeCard";
 
-const RecipeListing = ({ recipes, noAnimate }: { recipes: Recipe[]; noAnimate?: boolean }) => {
+const RecipeListing = ({
+  recipes,
+  noAnimate,
+  removeRecipeItem,
+}: {
+  recipes: Recipe[];
+  noAnimate?: boolean;
+  removeRecipeItem?: boolean;
+}) => {
   return (
     <div className="recipe-listing">
       {recipes.map((recipe: Recipe) => (
-        <RecipeCard noAnimation={noAnimate} key={recipe.id} recipe={recipe} />
+        <RecipeCard
+          removeRecipeFromFavorites={removeRecipeItem}
+          noAnimation={noAnimate}
+          key={recipe.id}
+          recipe={recipe}
+        />
       ))}
     </div>
   );
 };
-
 export default RecipeListing;
